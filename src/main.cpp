95c37c7868b242274a94322b8307d10b34d7b4bc
Matt McKenna <mtmckenna@gmail.com>
2025-05-22 21:13:51 -0700

    Revert "fix win opening txt files"
    This reverts commit cef1fa82ae9a4d2a6b8a0600fed9e7d43f8fa74f.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,3 @@
-#ifdef _WIN32
-#include <windows.h>
-#include <shellapi.h>
-#endif
-
 #include <iostream>
 #include <cassert>
 #include <fstream>
@@ -132,6 +127,9 @@ SDL_Renderer *renderer;
 bool show_settings = false;               // Global state variable to track the visibility of the settings view
 bool pin_selected_pwads_to_top = true;    // Global variable to track the pinning behavior
 static int selected_font_scale_index = 1; // Default to 1.0f (100%)
+
+#ifdef _WIN32
+#include <windows.h>
 
 void launch_process_win(const char *path)
 {
